09f2ed43c74f3be4a4afdb82809f8b06f1aba7c0
rookiesuri <rookiesuri@users.noreply.github.com>
2018-03-02 16:22:36 +0530

    simplified conn_string

---
+++ connect_db/01connect_db.py
@@ -0,0 +1,31 @@
+##connect_db
+##django-admin startproject connectpg starts project dont use .py
+
+import psycopg2
+
+##get credentials
+import ConfigParser
+Config = ConfigParser.ConfigParser()
+
+Config.read("C:\\Users\\soori\\Desktop\\client_secret\\credentials_db.ini")
+
+
+conn_string=Config.get('Section2','conn_string')
+print conn_string
+
+
+##########
+try:
+    conn = psycopg2.connect(conn_string)
+except:
+    print "I am unable to connect to the database"
+
+cur=conn.cursor()
+cur.execute("""select id,date_created from catalogue_product limit 10""")
+
+rows=cur.fetchall()
+# print rows
+print rows[0]
+print rows[0][0]
+print type(rows[0])
+print len(rows[0])
