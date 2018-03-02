89264108268a094cc0fdca2b582cf74fc6f0d635
rookiesuri <rookiesuri@users.noreply.github.com>
2018-03-02 14:05:17 +0530

    connect to postgre

---
+++ connect_db/connect_db.py
@@ -0,0 +1,19 @@
+##connect_db
+##django-admin startproject connectpg starts project dont use .py
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='spoyldev' user='marketing'host='spoyllivereaddb.cp1dn78gzqr5.ap-southeast-1.rds.amazonaws.com' password='S$tya#1296'")
+except:
+    print "I am unable to connect to the database"
+
+cur=conn.cursor()
+cur.execute("""select id,date_created from catalogue_product limit 10""")
+
+rows=cur.fetchall()
+# print rows
+print rows[0]
+print rows[0][0]
+print type(rows[0])
+print len(rows[0])
